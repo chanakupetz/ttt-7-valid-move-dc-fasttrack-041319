6f1db0d5cbc4d2a3cda038e29f018fcee38e2e63
Chana Kupetz <chanakupetz@gmail.com>
2019-04-14 16:20:47 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,7 +13,7 @@ def position_taken?(board,index)
 end
 
 def valid_move?(board,index)
-  if position_taken? && board[index].between?(0,8) == false
+  if position_taken? && index.between?(0,8) == false
   return true
   else
   return false
